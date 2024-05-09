481b16af438818e43731e17d6cbf123d08b0584c
nickfff-dev <hunchographics@gmail.com>
2024-05-09 20:11:11 +0300

    kue job processor

--- 0x03-queuing_system_in_js/7-job_processor.js
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -15,32 +15,16 @@ function sendNotification(phoneNumber, message, job, done) {
 
   // Check if the phone number is blacklisted
   if (blacklistedNumbers.includes(phoneNumber)) {
-    return done(new Error(`Phone number ${phoneNumber} is blacklisted`));
+    done(new Error(`Phone number ${phoneNumber} is blacklisted`));
+    return;
   }
 
   job.progress(50, 100);
   console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
-  job.progress(100, 100);
   done();
 }
 
 // Process the jobs
 queue.process('push_notification_code_2', 2, (job, done) => {
   sendNotification(job.data.phoneNumber, job.data.message, job, done);
-});
-
-// Log event on job completion
-queue.on('job complete', (id, result) => {
-  kue.Job.get(id, (err, job) => {
-    if (err) return;
-    console.log(`Notification job ${job.id} completed`);
-  });
-});
-
-// Log event on job failure
-queue.on('job failed', (id, errorMessage) => {
-  kue.Job.get(id, (err, job) => {
-    if (err) return;
-    console.log(`Notification job ${job.id} failed: ${errorMessage}`);
-  });
 });
